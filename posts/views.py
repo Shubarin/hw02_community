4d5e1c0e8d147fad9e6ca2819074b4730005c1a1
Кирилл Шубарин <kirill.shubarin@gmail.com>
2020-10-26 10:23:57 +0300

    add model community

---
+++ posts/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import get_object_or_404, render
+from .models import Group, Post
+
+
+def index(request):
+    latest = Post.objects.order_by("-pub_date")[:11]
+    context = {
+        "posts": latest
+    }
+    return render(request, "index.html", context)
+
+
+# view-функция для страницы сообщества
+def group_posts(request, slug):
+    # функция get_object_or_404 получает по заданным критериям объект
+    # из базы данных или возвращает сообщение об ошибке, если объект не найден
+    group = get_object_or_404(Group, slug=slug)
+
+    # Метод .filter позволяет ограничить поиск по критериям.
+    # Это аналог добавления условия WHERE group_id = {group_id}
+    posts = Post.objects.filter(group=group).order_by("-pub_date")[:12]
+    context = {
+        "group": group,
+        "posts": posts
+    }
+    return render(request, "group.html", context)
